ead1609abd8ac64378d5e01378d99efdb1dd47cc
Kirill Müller <kirill@cynkra.com>
2023-09-29 19:21:35 -0700

    check_pmbr()

---
+++ R/check-pmbr.R
@@ -0,0 +1,28 @@
+check_pmbr <- function(expr) {
+  switch(expr_type(expr),
+    call = {
+      # From Nimble user manual
+      dists <- c("dnorm", "dlnorm", "dt", "dbeta", "dgamma")
+      if (as.character(expr[[1]]) %in% dists) {
+        if (names2(as.list(expr))[[3]] != "sd") {
+          err("`", as.character(expr[[1]]), "` must be used with a named argument `sd`")
+        }
+      }
+      walk(as.list(expr)[-1], check_pmbr)
+    }
+  )
+}
+
+expr_type <- function(x) {
+  if (rlang::is_syntactic_literal(x)) {
+    "constant"
+  } else if (is.symbol(x)) {
+    "symbol"
+  } else if (is.call(x)) {
+    "call"
+  } else if (is.pairlist(x)) {
+    "pairlist"
+  } else {
+    typeof(x)
+  }
+}
